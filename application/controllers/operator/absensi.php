783e6dc7ab5f1d5ba9fcc7edfe5cdd59b4ab3766
Bry Jemi <bryand.bonaventura026@gmail.com>
2020-10-20 01:27:32 +0700

    rename file name

--- application/controllers/operator/absensi.php
+++ application/controllers/operator/absensi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class absensi extends CI_Controller {
+class Absensi extends CI_Controller {
 
     public function __construct()
     {
--- application/controllers/operator/dosen.php
+++ application/controllers/operator/dosen.php
@@ -2,7 +2,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class dosen extends CI_Controller {
+class Dosen extends CI_Controller {
 
     public function __construct()
     {
